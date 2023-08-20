be266fd20f477dceba5c0a6dc2cae23e87fb3480
kazi <kazi@localhost.localdomain>
2023-08-20 21:05:22 +0530

    dumb component

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useSate("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,35 +1,17 @@
 import "./ExpenseItem.css";
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
-
-  const deleteExpense = () => {
-    console.log("delete expense!");
-  };
-
-  const editExpense = () => {
-    setAmount(100);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <h3>{props.LocationOfExpenditure}</h3>
         <div className="expense-item__price">${amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
-        <button onClick={deleteExpense}>Delete Expense</button>
-        <button onClick={editExpense}>Edit Expense</button>
       </div>
     </Card>
   );
